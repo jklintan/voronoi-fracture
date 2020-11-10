040cd311b448b6d3b694d330c35a94905f845e64
linusmossberg <linusmossbergmt@gmail.com>
2020-11-10 11:53:09 +0100

    Add mesh selection

--- source/voronoi-fracture.cpp
+++ source/voronoi-fracture.cpp
@@ -1,6 +1,12 @@
 #include "voronoi-fracture.h"
 
 #include <maya/MGlobal.h>
+#include <maya/MDagPath.h>
+#include <maya/MObject.h>
+#include <maya/MFnDagNode.h>
+#include <maya/MItSelectionList.h>
+#include <maya/MItMeshVertex.h>
+#include <maya/MFnMesh.h>
 
 VoronoiFracture::VoronoiFracture() {};
 
@@ -8,9 +14,47 @@ VoronoiFracture::~VoronoiFracture() {};
 
 MStatus VoronoiFracture::doIt(const MArgList& args)
 {
-    MGlobal::displayInfo("This function will fracture");
+    MSelectionList list;
+    MGlobal::getActiveSelectionList(list);
 
-    return MStatus(MStatus::MStatusCode::kSuccess);
+    MItSelectionList it(list, MFn::kMesh);
+    if (it.isDone())
+    {
+        displayError("Polygonal mesh must be selected before fracturing.");
+        return MS::kFailure;
+    }
+
+    // Use the first kMesh object in the selection
+    MObject component;
+    MDagPath node;
+    it.getDagPath(node, component);
+
+    MFnDagNode node_fn(node);
+    displayInfo(MString("Fracturing ") + node_fn.name());
+
+    MObject mesh = node_fn.object();
+
+    MStatus status;
+    MItMeshVertex vertex_it(mesh, &status);
+    if (!status)
+    {
+        displayError("Invalid mesh selected");
+        return status;
+    }
+
+    // Just print the vertices of the selected object for now
+    for (; !vertex_it.isDone(); vertex_it.next())
+    {
+        MPoint p = vertex_it.position(MSpace::kWorld);
+
+        displayInfo((
+            std::to_string(p.x) + ", " + 
+            std::to_string(p.y) + ", " + 
+            std::to_string(p.z)).c_str()
+        );
+    }
+    
+    return MS::kSuccess;
 }
 
 void* VoronoiFracture::creator()
